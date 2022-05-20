9aa64a02b77f7b57f59c78fc1061a2b875c697bb
parisote <detomas@gmail.com>
2022-05-19 22:26:29 -0300

    fix: name model

--- src/db/models/stepcomponents.js
+++ src/db/models/stepcomponents.js
@@ -3,7 +3,7 @@ const {
   Model
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class stepComponent extends Model {
+  class stepComponents extends Model {
     /**
      * Helper method for defining associations.
      * This method is not a part of Sequelize lifecycle.
@@ -13,13 +13,13 @@ module.exports = (sequelize, DataTypes) => {
       // define association here
     }
   }
-  stepComponent.init({
+  stepComponents.init({
     recipeId: DataTypes.INTEGER,
     ingredientId: DataTypes.INTEGER,
     ingredientAmount: DataTypes.DECIMAL(10,2)
   }, {
     sequelize,
-    modelName: 'stepComponent',
+    modelName: 'stepComponents',
   });
-  return stepComponent;
+  return stepComponents;
 };
--- src/db/seeders/20220520005658-stepComponent.js
+++ src/db/seeders/20220520005658-stepComponent.js
@@ -2,13 +2,13 @@
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.bulkInsert('step_component', [{
+    return queryInterface.bulkInsert('step_components', [{
       recipeId: 1,
       ingredientId: 1,
       ingredientAmount: 10
     }]);
   },
   down: (queryInterface, Sequelize) => {
-    return queryInterface.bulkDelete('step_component', null, {});
+    return queryInterface.bulkDelete('step_components', null, {});
   }
 };
