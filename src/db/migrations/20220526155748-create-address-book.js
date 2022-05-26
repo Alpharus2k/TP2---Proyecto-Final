93344848a7691b6963c6b03fa80362984b605386
AlphaRus2k <alpharus2@gmail.com>
2022-05-26 13:04:18 -0300

    Creada Migration y Model de la tabla addressBook

---
+++ src/db/migrations/20220526155748-create-address-book.js
@@ -0,0 +1,36 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('addressBooks', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.DataTypes.INTEGER.UNSIGNED
+      },
+      poepleId: {
+        type: Sequelize.DataTypes.INTEGER.UNSIGNED,
+        allowNull: false
+      },
+      addressId: {
+        type: Sequelize.DataTypes.INTEGER.UNSIGNED,
+        allowNull: false
+      },
+      createdAt: {
+        type: Sequelize.DataTypes.DATE,
+        allowNull: true
+      },
+      updatedAt: {
+        type: Sequelize.DataTypes.DATE,
+        allowNull : true
+      },
+      deletedAt: {
+        type: Sequelize.DataTypes.DATE,
+        allowsNull: true
+    }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('addressBook');
+  }
+};
