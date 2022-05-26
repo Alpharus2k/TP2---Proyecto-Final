aaca8d0de2b012b722db2a255ac424b30c851496
AlphaRus2k <alpharus2@gmail.com>
2022-05-26 14:39:38 -0300

    Creada Migration y Model de la tabla Recipes

---
+++ src/db/models/recipes.js
@@ -0,0 +1,25 @@
+'use strict';
+const {Model} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Recipes extends Model {
+   
+    static associate(models) {
+      // define association here
+    }
+  }
+  Recipes.init({
+    name: {
+      type: Sequelize.DataTypes.STRING(100),
+      allowNull: false
+    },
+    description: {
+      type: Sequelize.DataTypes.STRING(200),
+      allowNull: true
+    }
+  }, {
+    sequelize,
+    modelName: 'Recipes',
+    tableName: 'recipes',
+  });
+  return Recipes;
+};
