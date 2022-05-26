f15d5328a582d813c3bc9459630fba6e7f4a1057
AlphaRus2k <alpharus2@gmail.com>
2022-05-26 12:15:25 -0300

    Creado el seeder de foodHazardsIngredients

--- src/db/seeders/20220526035958-foodHazardsIngredients.js
+++ src/db/seeders/20220526035958-foodHazardsIngredients.js
@@ -2,15 +2,38 @@
 
 module.exports = {
   async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('foodHazardsIngredients', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+    const list =[{
+      ingredientId: 1,
+      foodDangersId: 3
+    },{
+      ingredientId: 2,
+      foodDangersId: 3
+    },{
+      ingredientId: 3,
+      foodDangersId: 2
+    },{
+      ingredientId: 4,
+      foodDangersId: 8
+    },{
+      ingredientId: 5,
+      foodDangersId: 1
+    },{
+      ingredientId: 5,
+      foodDangersId: 2
+    },{
+      ingredientId: 5,
+      foodDangersId: 3
+    },{
+      ingredientId: 5,
+      foodDangersId: 8
+    },{
+      ingredientId: 6,
+      foodDangersId: 1
+    }
+  ];
+    
+    await queryInterface.bulkInsert('foodHazardsIngredients', list, {});
+  
   },
 
   async down (queryInterface, Sequelize) {
