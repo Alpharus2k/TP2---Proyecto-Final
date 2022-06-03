54da1661ef3758459268ebcb55d752235b580152
Juan Martinez <jmartinez@soluciones.com.ar>
2022-06-02 23:36:21 -0300

    relaciones entre cliente, addressBook y addresses

--- src/db/models/client.js
+++ src/db/models/client.js
@@ -10,7 +10,9 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
+      Client.belongsToMany(models.addressBook, {
+        through: 'client_addressBook'
+      })
     }
   }
   Client.init({
